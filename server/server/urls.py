9b217d69fb40188cdfb8cf2f8800362ea422d677
Gowtham-jk <gowthamsec25@gmail.com>
2023-12-20 13:55:19 +0530

    sucess

---
+++ server/server/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('areaofprism/',views.prismarea,name="areaofprism"),
+    path('',views.rectarea,name="areaofrectangleroot")
+]
